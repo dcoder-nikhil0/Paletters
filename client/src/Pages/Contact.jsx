b8e6468663c0f2243e91aa06c76d15fbc21710ab
DcoderLLC <dcoderllc@gmail.com>
2025-03-23 01:11:45 +0530

    Contact Form Routed

---
+++ client/src/Pages/Contact.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import  Form  from '../Components/Contact/Form'
+
+const Contact = () => {
+  return (
+    <Box>
+      <Form />
+    </Box>
+  )
+}
+
+export default Contact
