bce9f05b85a5c470f489a75bcc38fa79bcc0773e
DcoderLLC <dcoderllc@gmail.com>
2025-03-25 09:48:41 +0530

    About Hero Updated

--- client/src/Components/About/AboutHero.jsx
+++ client/src/Components/About/AboutHero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid, Box, Typography } from "@mui/material";
-import bg from "../../assets/bg.webp"
+import bg from "../../assets/assest.png"
 
 const AboutHero = () => {
   return (
