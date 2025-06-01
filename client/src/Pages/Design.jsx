56ef34553740dce4fb3bd471709c4c726a97238f
tech-paletterse <paletterse@gmail.com>
2025-06-01 23:34:50 +0530

    Desing Page done

--- client/src/Pages/Design.jsx
+++ client/src/Pages/Design.jsx
@@ -5,17 +5,13 @@ import DesignOffers from '../Components/Design/DesignOffers'
 import WhyChooseUs from './../Components/WhyChooseUs';
 import Form from '../Components/Contact/Form';
 import DesignComparison from '../Components/Design/DesignComparison';
-import DesignCTA from '../Components/Design/DesignCTA';
-import DesignShowcase from '../Components/Design/DesignShowCase';
 
 const Design = () => {
   return (
     <Box>
       <DesignHero />
       <DesignOffers />
-      <DesignShowcase />
       <DesignComparison />
-      <DesignCTA />
       <WhyChooseUs />
       <Form />
     </Box>
