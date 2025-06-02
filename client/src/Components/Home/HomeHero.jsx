96201e5113b8d3c200ac4d670792f67a050655f3
tech-paletterse <paletterse@gmail.com>
2025-06-03 02:49:00 +0530

    Home page final

--- client/src/Components/Home/HomeHero.jsx
+++ client/src/Components/Home/HomeHero.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import Slider from "react-slick";
